14e3c672d19d1899374b027fec240a307638506d
aggelito <augustherrstrom@gmail.com>
2014-03-06 12:59:11 +0100

    Feffe och Agge
    Scene select + scene select in game + inforuta. fungerar för android

---
+++ BaseGame/Mikonipi/Assets/BackToStartScreenMikonipi.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackToStartScreenMikonipi : MonoBehaviour {
+		TouchButton tb;
+		public GameObject obj_tb;
+	// Use this for initialization
+	void Start () {
+				tb = obj_tb.GetComponent<TouchButton> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+				if (tb._buttonEnabled == true) {
+						Application.LoadLevel ("StartScen");
+				}
+	}
+}
