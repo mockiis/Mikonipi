723cb6a50d92848609e8c2393fecd76a20f7754f
FeffCodeMaster <skoog_fredrik@hotmail.se>
2014-03-31 14:05:26 +0200

    Alpha och Xml
    Fixat med Alpha i Grid

---
+++ BaseGame/Mikonipi/Assets/Script/XmlBaseObject.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class XmlBaseObject : MonoBehaviour {
+
+
+		public string name;
+		public string time;
+		public float rank;
+
+		public GameObject BaseObject;
+
+	void Start () {
+			
+	
+	}
+	
+	// Update is called once per frame
+		void Update () {
+
+
+	
+	}
+
+}
